adf6482a18b3e95c03ac084ab8be52330b19b068
TanzRif <47933652+TanzRif@users.noreply.github.com>
2020-04-16 10:43:14 -0400

    Add files via upload

---
+++ src/test/java/com/maven/demo/mavenBatch4demo/FinancialLoan_Test.java
@@ -0,0 +1,12 @@
+package com.maven.demo.mavenBatch4demo;
+
+import org.junit.Test;
+
+public class FinancialLoan_Test {
+	
+	@Test
+	public void Financialonline() {
+		System.out.println("financialOnline");
+	}
+
+}
